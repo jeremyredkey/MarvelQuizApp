6a067d05e84056fbc07a2a13c226252d8d26af98
Jeremy Redkey <jeremy.redkey@gmail.com>
2020-07-23 02:34:29 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,174 @@
+function updateQuestionNumber() {
+  STORE.currentQuestion++;
+  $('#questionNumber').html(`${STORE.currentQuestion}`)
+}
+
+/* Displays the options for the current question */
+function render() {
+  //Need to generate question for user
+  let html = "";
+  if (STORE.page === "Landing Page") {
+    html = `<div id="quiz-container" class="container text-center card">
+       <p class="lead">Are you nerdy enough to call yourself a superfan of Marvel? <br> Then put your knowledge to the test with this quiz!</p>
+       <button id="startBtn" type="button" class="btn btn-primary btn-lg">Start Quiz Now <i
+           class="fas fa-angle-right"></i></button>
+       </div>
+   </div>`
+  }
+  else if (STORE.page === "Question") {
+
+    let renderQuestion = STORE.questions[STORE.currentQuestion]
+    let thisQuestion = renderQuestion.question;
+    // console.log(thisQuestion)
+    html = `            
+    <div id="quiz-container" class="container text-center card">
+                <div class="row">
+                   <div class="col">
+                   <form>
+                    <div id="feedback"></div>
+                       <!-- Quiz question -->
+                       <div id="question">
+                       <p class="lead">${thisQuestion}</p>
+                       </div>
+                       <div id="options">
+                       <!-- Where options will populate -->
+                        ${updateOptions()}
+                       </div>
+                       <hr>
+                       <button id="submitBtn" type="submit" class="btn btn-primary btn-lg">Submit Answer <i
+                               class="fas fa-angle-right"></i></button>
+                              
+                               </form>
+                  </div>
+                  </div>
+                  `;
+
+
+  } else if (STORE.page === "Feedback") {
+    const { selectedOption, indexQuestion } = STORE
+
+    let correctHtml = `<div class="alert alert-success" role="alert"><i class="fas fa-check"></i>
+          <strong>${indexQuestion.answer}</strong> is correct, you scored one point!
+            </div>`;
+
+    let incorrectHtml = `
+          <div class="alert alert-danger" role="alert">
+  <i class="fas fa-times"></i> That answer is incorrect, you did not score a point! The answer was <strong>${indexQuestion.answer}</strong>.
+</div>`
+    if (selectedOption !== indexQuestion.answer) {
+      html = incorrectHtml
+
+    } if (selectedOption === indexQuestion.answer) {
+      html = correctHtml
+
+
+    }
+    
+    html += `<button id="nextButton">Next Question</button>`
+  }
+  else if (STORE.page === "Completed") {
+      html = `
+      <button id="startBtn">Restart</button>
+      `
+    }
+  html = `
+  <div id="scoresQuestionNumDiv">  
+            <h2><small>Questions Number: <span id="questionNumber">${STORE.currentQuestion + 1}</span> of 15 | Score: <span
+                        id="quizScore">${STORE.score}</span>/15
+                </small></h2>
+            </div>` + html;
+  $('main').html(html)
+};
+
+
+function updateOptions() {
+  let optionsHtml = "";
+  //setting index value
+  let question = STORE.questions[STORE.currentQuestion];
+
+  for (let i = 0; i < question.options.length; i++) {
+    optionsHtml += `
+          <!-- Start of all the available answers -->
+          <div class="form-check">
+              <input id="option${i + 1}" class="form-check-input" name="options" type="radio" value="${question.options[i]}" tabindex ="${i + 1}" required>
+              <label for="option${i + 1}" class="form-check-label">
+                  ${question.options[i]} <br>
+              </label>
+          </div>
+      `
+  }
+  return `${optionsHtml}` 
+}
+
+//
+function eventHandlers() {
+  $("main").on("click", "#startBtn", e => {
+    STORE.page = "Question";
+    STORE.currentQuestion = 0;
+    STORE.score = 0;
+    render()
+  })
+  $("main").on("click", "#nextButton", e => {
+    
+    if(STORE.currentQuestion < 14 ) {
+STORE.currentQuestion++
+      STORE.page = "Question";
+    } else {      
+      STORE.page = "Completed";
+    }
+    
+ 
+    render()
+  })
+}
+
+
+function submitAnswer() {
+  $("main").on('submit', 'form', e => {
+
+    e.preventDefault();
+
+    STORE.indexQuestion = STORE.questions[STORE.currentQuestion];
+    STORE.selectedOption = $(".form-check-input:checked").val();
+        if (STORE.selectedOption === STORE.indexQuestion.answer) {
+    STORE.score++
+
+    }
+
+    STORE.page = "Feedback";
+    render();
+
+
+  });
+}
+
+
+function updateScore() {
+  STORE.score++;
+  $('#quizScore').html(`${STORE.score}`)
+
+}
+
+
+
+
+
+/* displays results and restart quiz button */
+
+
+
+/* checks whether it reached the end of questions list */
+
+
+/*checks whether the chosen option is right or wrong and displays respective message*/
+
+// startQuiz function needs to be created
+/* when a user clicks on start quiz button */
+function startQuiz() {
+  render()
+  eventHandlers()
+  updateOptions()
+  submitAnswer()
+};
+
+$(startQuiz)
---
+++ store.js
@@ -0,0 +1,180 @@
+const STORE = {
+  currentQuestion: 0,
+  score: 0,
+  page: "Landing Page", 
+  questions: [//1
+      {
+        question: 'What is the strongest material in the MCU?',
+        options: [
+          "Proto-Adamantium", 
+          "Kryptonite", 
+          "Vibranium", 
+          "Enchanted uru",
+          "None of the Above"
+        ],
+        answer: "Proto-Adamantium"
+      },
+      //2
+      {
+        question: 'What is the real name of Black Widow?',
+        options: [
+          "Vanessa Valalski",
+          "Natalia Romanova", 
+          "Natasha Romanoff",
+          "Scarlett Johansson",
+          "None of the Above"
+        ],
+        answer: "Natasha Romanoff"
+      },
+      //3
+      {
+        question: 'In which film do we finally learn the backstory behind how Nick Fury lost his eye?',
+        options: [
+          "Captain America: The Winter Soldier", 
+          "Ironman 1", 
+          "Captain America: Civil War", 
+          "Captain Marvel",
+          "None of the Above"
+        ],
+        answer: "Captain Marvel"
+      },
+      //4
+      {
+        question: "What is Deadpool's favorite food?", 
+        options: ["Cheeseburger", "Chimichangas", "Pizza", "Pancakes", "None of the Above"],
+        answer: "Chimichangas"
+      },
+      //5
+      {
+        question: "What is the name of the axe created for and then used by Thor in Avengers: Infinity War?",
+        options: [
+          "Windwrecker", 
+          "Mjolnir", 
+          "Gungnir", 
+          "Stormbreaker",
+          "None of the Above"
+        ],
+        answer: "Stormbreaker"
+      },
+      //6
+      {
+        question: "What is the infamous villian in the next Venom movie?",
+        options: [
+          "Carnage", 
+          "Sandman", 
+          "Lizard", 
+          "Doctor Octopus",
+          "None of the Above"
+        ],
+        answer: "Carnage"
+      },
+      //7
+      {
+        question: "What song do we hear first in Guardians of the Galaxy?",
+        options: [
+          "Come and Get Your Love by Redbone", 
+          "Bring It On Home to Me by Sam Cooke", 
+          "I'm Not In Love by 10cc", 
+          "Don't Stop Me Now by Queen",
+          "None of the Above"
+        ],
+        answer: "I'm Not In Love by 10cc"
+      },
+      //8
+      {
+        question: "How many Iron Man movies are there?", 
+        options: [
+          "5", 
+          "4", 
+          "3", 
+          "2",
+          "None of the Above"
+        ],
+        answer: "3"
+      },
+      //9
+      {
+        question: "What MCU movie starring Jared Leto is supposed to release this year?",
+        options: [
+          "Blade", 
+          "Punisher", 
+          "Suicide Squad", 
+          "Morbius",
+          "None of the Above"
+        ], 
+        answer: "Morbius"
+      },
+      //10
+      {
+        question: "How does the character Blade become half-vampire?",
+        options: [
+          "His mother was bitten by a vampire when giving birth", 
+          "Was given vampire blood ritually", 
+          "Was bitten by a vampire passing on vampire enzymes", 
+          "He made a deal with Dracula",
+          "None of the Above"
+        ],
+        answer: "His mother was bitten by a vampire when giving birth"
+      },
+      //11
+      {
+        question: "In what film's post-credit scene did Thanos first appear?",
+        options: [
+          "Avengers", 
+          "Avengers: Age of Ultron", 
+          "Ironman 2",
+          "Ironman 3", 
+          "None of the above"
+        ],
+        answer: 'Avengers: Age of Ultron'
+      },
+      //12
+      {
+        question: "What is the species does Loki originally hail?", 
+        options: [
+          "Asgardian", 
+          "Frost Giants", 
+          "Demon", 
+          "Vanir",
+          "None of the Above"
+        ],
+        answer: "Frost Giants"
+      },
+      //13
+      {
+        question: "Who is the villain in Spiderman: Far From Home?",
+        options: [
+          "Lizard", 
+          "Green Goblin", 
+          "Doctor Octopus", 
+          "Mysterio",
+          "None of the Above"
+        ],
+        answer: "Mysterio"
+      },
+      //14
+      {
+        question: "Who is the voice of Rocket?",
+        options: [
+          "Brad Pitt", 
+          "Bradley Cooper", 
+          "Vin Diesel",
+          "Morgan Freeman", 
+          "None of the Above"
+        ],
+        answer: "Bradley Cooper"
+      },
+      //15
+      {
+        question: "What did Yondu refer to himself in the last Guardians of the Galaxy?",
+        options: [
+          "Mary Poppins", 
+          "A space pirate", 
+          "A straight shooter",
+          "A good for nothing scoundrel", 
+          "All of the Above"
+        ],
+        answer: "Mary Poppins"
+      }
+    ],
+  };
